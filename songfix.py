586f9b057f1df479631c9162a6bdc352b9b983b5
StrongVStrong <bha2095@gmail.com>
2024-12-17 18:44:07 -0500

    Remake
    Had to remake BASICALLY THE ENTIRE THING for it to support slash commands

--- songfix.py
+++ songfix.py
@@ -7,13 +7,23 @@ directory = r'C:\Users\Megas\Documents\GitHub\GuessDokkanOst\songs'
 for filename in os.listdir(directory):
     # Check if it's a file (to avoid directories)
     if os.path.isfile(os.path.join(directory, filename)):
+        # Start with the original filename
+        new_filename = filename
+        
         # Find the position of the first "-" in the filename
-        dash_index = filename.find('-')
+        dash_index = new_filename.find('-')
         
         # If there's a "-" in the filename, slice the filename to remove everything after it
         if dash_index != -1:
-            new_filename = filename[:dash_index].strip() + os.path.splitext(filename)[1]  # Retain the file extension
-            # Rename the file
+            new_filename = new_filename[:dash_index].strip() + os.path.splitext(new_filename)[1]  # Retain the file extension
+        
+        # If there's a "Dragon Ball Z Dokkan Battle: " in the filename, remove it
+        prefix = "Dragon Ball Z Dokkan Battle： "
+        if prefix in new_filename:
+            new_filename = new_filename.replace(prefix, "")
+        
+        # Rename the file only if the filename has changed
+        if new_filename != filename:
             os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
 
 print("Renaming complete.")
